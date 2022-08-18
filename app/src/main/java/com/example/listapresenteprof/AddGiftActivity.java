5d2c4e2249aa89efa5abdf3f63b57053f1ee6bbd
mmfernandes <may.fernandes25@hotmail.com>
2022-08-17 22:03:06 -0300

    aula 17/08 primeira edição da lista de presente

---
+++ app/src/main/java/com/example/listapresenteprof/AddGiftActivity.java
@@ -0,0 +1,14 @@
+package com.example.listapresenteprof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddGiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gift);
+    }
+}
--- app/src/main/java/com/example/listapresenteprof/MainActivity.java
+++ app/src/main/java/com/example/listapresenteprof/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.listapresenteprof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FloatingActionButton buttonAddGift;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAddGift = findViewById(R.id.button_add);
+
+
+        buttonAddGift.setOnClickListener
+                (new View.OnClickListener()
+                 {
+                     @Override
+                    public void onClick(View v)
+                    {
+                        Intent intent = new Intent(
+                                getApplicationContext(), //contexto da aplicação parametro 1
+                                AddGiftActivity.class // classe que a gente quer chama parametro2
+                        );
+                        startActivity(intent);
+                    }
+                  }
+                );
+
     }
 }
+
+//se eu fosse o professor eu começava a vir de regata
